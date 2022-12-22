ebbc479b11b6735e608059a793a19feae19bd5f4
Yurevych Nataliia <116509687+yurevych2@users.noreply.github.com>
2022-12-22 15:01:08 +0200

    Merge final_solution_one_el to develope

--- recurrent_equations.py
+++ recurrent_equations.py
@@ -89,12 +89,30 @@ def get_coefs() -> str:
     return final
 
 def final_solution_one_el() -> float:
+    def final_solution_one_el() -> float:
     '''
     Documentation and docstring.
     Підставляє r, А в загальне рівняння А_н. Повертає фінальний розв'язок А_н.
     МАРТА
+    >>> final_solution_one_el()
     '''
-    pass
+    equation = analytic_solution()
+    Rs = find_roots()
+    As = get_coefs()
+    #r = [829482394823,2,9839283,4]
+    #As = [56,2,3,4]
+    #equation = 'A1*r1^(n-1) + A2*r2^(n-2) + A3*r3^(n-3) + A4*r4^(n-4)'
+
+    if len(Rs) != len(equation.split(' + ')) or len(As)!= len(equation.split(' + ')):
+        return 'Kinda cringe bro'
+
+    for el in range(len(As)):
+        equation = equation.replace(f'A{el+1}',str(As[el]))
+
+    for xd in range(len(Rs)):
+        equation = equation.replace(f'r{xd+1}',str(Rs[xd]))
+
+    return equation
 
 def final_solution_plural_el(nums: list) -> list[float]:
     '''
