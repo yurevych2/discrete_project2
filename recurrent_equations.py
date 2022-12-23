256f5a242800e65b3b81fb9459e9a545c25be220
Yurevych Nataliia <116509687+yurevych2@users.noreply.github.com>
2022-12-23 19:09:53 +0200

    Update recurrent_equations.py

--- recurrent_equations.py
+++ recurrent_equations.py
@@ -1,17 +1,15 @@
 '''
 Modul that solve reccurent equations in two ways: slow and speed.
 Also you can get analysis of work of functions.
-
-EXAMPLE OF USAGE
-python recurrent_equations.py '3*a(n-1) + 10*a(n-2)' 1, 43 --speed_search 2
 '''
 
 import argparse
+from timeit import timeit
 from copy import deepcopy
 from numpy import array, matmul
 from numpy.linalg import matrix_power, det
 
-from find_roots import get_root, coincidence_of_roots
+from find_roots import get_root
 
 def modify_equation(equation_: str) -> str:
     '''
@@ -31,6 +29,7 @@ def modify_equation(equation_: str) -> str:
         equation = equation.replace(' - ', ' + -')
     
     return equation
+
 
 def general_solution(equation_: str) -> tuple:
     '''
@@ -54,51 +53,55 @@ def general_solution(equation_: str) -> tuple:
 
     return equation, coeff
 
-# def find_roots() -> list[float]: FUNCTION get_root and coincidence_of_roots
 
 def replace_r(equation_: str, roots: list):
     '''
     Replace r with its value.
 
-    >>> replace_r('A1*r^(n-1) + A2*r^(n-2) + A3*r^(n-3)', [1, 3, 5])
-    'A1*1**n + A2*3**n + A3*5**n'
+    >>> replace_r('A1*r^(n-1) + A2*r^(n-2) + A3*r^(n-3)', [1, 3, 3])
+    'A1*1**n + A2*3**n + A3*3**n*n**1'
     '''
     equation = deepcopy(equation_)
     
     i = 0
+    used = []
     while i < len(roots):
-        equation = equation.replace(f'r^(n-{i+1})', f'{roots[i]}**n')
+        used.append(roots[i])
+        equation = equation.replace(f'r^(n-{i+1})', f'{roots[i]}**n*n**{used.count(roots[i])-1}')
+        equation = equation.replace('*n**0', '')
         i += 1
     
     return equation
+
 
 def get_system(equation_: str, base: list[int]):
     '''
     :param base list[int]: basic elements the n-th element depends on it.
 
     >>> get_system('A1*1**n + A2*3**n + A3*5**n', [3, 5])
-    ('1*A1 + 3*A2 + 5*A3 = 3', '1*A1 + 9*A2 + 25*A3 = 5')
+    ('1*A1 + 1*A2 + 1*A3 = 3', '1*A1 + 3*A2 + 5*A3 = 5')
     '''
-    equation = deepcopy(equation_)
 
     result = []
     for index, num in enumerate(base):
-        equation_ = deepcopy(equation)
-        equation_ = equation_.replace('n', str(index + 1))
-        equation_ = equation_.split(' + ')
+        equation = deepcopy(equation_)
+        equation = equation.replace('n', str(index))
+        equation = equation.split(' + ')
 
         powered_equation = []
-        for part in equation_:
+        for part in equation:
             index = part.find('*')
-            number = eval(part[index+1:])
+            sl = part[index+1:]
+            number = eval(sl)
             powered_equation.append(f'{number}*{part[:index]}')
 
-        equation_ = ' + '.join(powered_equation) + f' = {num}'
-        result.append(equation_)
+        equation = ' + '.join(powered_equation) + f' = {num}'
+        result.append(equation)
     
     return tuple(result)
 
-def get_coefs(equation_: str) -> str:
+
+def get_coefs(system: tuple) -> str:
     '''
     Fucntion returns roots of equation system using Cramer`s method.
     >>> get_coefs(( '2*A1 + 5*A2 = 19', \
@@ -111,15 +114,15 @@ def get_coefs(equation_: str) -> str:
     '''
     # definition coeff
     coeff = []
-    for i in range(len(equation_)):
+    for i in range(len(system)):
         row  = []
-        a = equation_[i].split(' + ')
+        a = system[i].split(' + ')
         for j in range(len(a)):
             row.append(int(a[j].split('*')[0]))
         coeff.append(row)
 
     # definition res   
-    res = [int(equation_[i].split(' = ')[1]) for i in range(len(equation_))]
+    res = [int(system[i].split(' = ')[1]) for i in range(len(system))]
 
     def rmatrix(n):
         pro = deepcopy(coeff)
@@ -129,14 +132,15 @@ def get_coefs(equation_: str) -> str:
         return pro
 
     # matrix determinant
-    det_ = [det(array(rmatrix(n))) for n in range(len(equation_)+1)]
+    det_ = [det(array(rmatrix(n))) for n in range(len(system)+1)]
 
     #roots
-    final = [round(det_[u]/det_[0],2) for u in range(1, len(equation_)+1)]
+    final = [round(det_[u]/det_[0],2) for u in range(1, len(system)+1)]
 
     return final
 
-def final_solution_one_el(equation_, As) -> float:
+
+def final_solution_one_el(equation_: str, As: list) -> float:
     '''
     Substitutes r, A and returns the final solution of the nth term.
 
@@ -151,19 +155,24 @@ def final_solution_one_el(equation_, As) -> float:
     
     for i in range(0,len(equation.split())):
         equation = equation.replace(f'(n-{i})','n')
+    
+    equation = equation.replace('**', '^')
+    equation = equation.replace("*", "*(")
+    equation = equation.replace("^", ")^")
+    equation = equation.replace('^', '**')
 
     return equation
 
-def final_solution_plural_el(nums: list) -> list[float]:
-    '''
-    :param nums list: numbers of elements to calculate.
-    '''
-    solutions_lst = []
-    for element in nums:
-        solution = final_solution_one_el(element)
-        solutions_lst.append(solution)
 
-    return solutions_lst
+def final_solution_plural_el(final_solution: str, nums: int, basa: list) -> list[float]:
+    solution = deepcopy(final_solution)
+    solutions_lst = []
+    for num in range(len(basa), nums+1):
+        value_solution = solution.replace('n', str(num))
+        solutions_lst.append(eval(value_solution))
+
+    return basa + solutions_lst
+
 
 def get_transitive_matrix(equation_: str):
     '''
@@ -186,6 +195,7 @@ def get_transitive_matrix(equation_: str):
 
     return matrix
 
+
 def get_nth_el(equation_: str, first_els: list, n: int):
     '''
     Find n-th element of reccurence relationship.
@@ -205,30 +215,34 @@ def get_nth_el(equation_: str, first_els: list, n: int):
         n_th = list(matmul(array(first_els), matrix))[-1]
         return n_th
 
-def speed_analisys():
-    '''
-    Documentation and docstring.
-    НАТАЛІ
-    '''
-    pass
-
-parser = argparse.ArgumentParser(description='Solve reccurent equations in \
-    two ways: slow and speed. Compare those ways.')
-
-parser.add_argument('equation', type=str, help='The right part of equation you want to solv. \
-    E.g. "4*a(n-1) + 2*a(n-2) + -6*a(n-3)"')
-parser.add_argument('base_elements', type=float, nargs='+', help='The elements the a_n depends on. \
-    E.g. list with length of 3 first values for "4*a(n-1) + 2*a(n-2) + -6*a(n-3)"')
-
-parser.add_argument('--speed_search', type=int, \
-    help='Find N-th (start from 0) element of the equation in O(logN)')
-
-# choices=[0, 1, 2]
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    # import doctest
+    # doctest.testmod()
 
-    args = parser.parse_args()
-    if args.speed_search:
-        print(get_nth_el(args.equation, args.base_elements, args.speed_search))
+    # slow
+    start = timeit()
+
+    equation = '3*a(n-1) + 10*a(n-2)'
+    num = 100
+    base = [4, 1]
+    mod = modify_equation(equation)
+    gen0 = general_solution(mod)[0]
+    gen1 = general_solution(mod)[1]
+    roots = get_root(gen1, 0.1)
+    rep = replace_r(gen0, roots)
+    system = get_system(rep, base)
+    coefs = get_coefs(system)
+    fin = final_solution_one_el(rep, coefs)
+
+    print(f'100-th (slow) = {final_solution_plural_el(fin, num, base)[-1]}')
+    end = timeit()
+    
+    print(f'Time to find 100 elements (slow) if a_n = 3*a(n-1) + 10*a(n-2):\n{end - start}')
+
+    # analysis and speed
+    start2 = timeit()
+    print(f'\n\n100-th (speed) = {get_nth_el(equation, base, 100)}')
+    end2 = timeit()
+
+    print(f'Time to find 100-th element (speed) if a_n = 3*a(n-1) + 10*a(n-2):\n{end2 - start2}')
