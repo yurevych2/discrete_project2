59390ce3fdf226b6192370944ecbc3548ca8787d
VikaProkhorova <116552538+VikaProkhorova@users.noreply.github.com>
2022-12-23 14:05:41 +0200

    Update recurrent_equations.py

--- recurrent_equations.py
+++ recurrent_equations.py
@@ -41,7 +41,7 @@ def final_solution_plural_el(nums: list) -> list[float]:
     ВІКА
     '''
     solutions_lst = []
-    for element in nums:
+    for element in range(num):
         solution = final_solution_one_el(element)
         solutions_lst.append(solution)
     return solutions_lst
